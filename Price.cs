7e09018d4f88aac59fb3a879b8b1c3b67be36f9d
labibahhoque <hoquelabibah@gmail.com>
2023-05-19 22:45:37 +0600

    labibah

---
+++ Price.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trial_transportation
+{
+    internal class Price
+    {
+        public double Chittagong = 1400;
+        public double Dhaka = 1200;
+        public double Barisal = 1500;
+        public double Sylhet = 1300;
+        public double Rajshahi = 1100;
+
+        public double Business = 500;
+        public double Economy = 200;
+
+        public double USBangla = 100;
+        public double BimanBangladesh = 50;
+        public double NovoAir = 80;
+    }
+}
