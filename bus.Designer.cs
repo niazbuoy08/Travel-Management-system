bc131bff7354e163c0d7528240a68ed1079abf52
niazbuoy08 <rahmanniaz29@gmail.com>
2023-05-20 12:30:02 +0600

    10th commit

--- bus.Designer.cs
+++ bus.Designer.cs
@@ -68,6 +68,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(58, 515);
             this.label8.Name = "label8";
@@ -89,6 +90,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(54, 454);
             this.label7.Name = "label7";
@@ -188,6 +190,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(58, 371);
             this.label5.Name = "label5";
@@ -198,6 +201,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(58, 290);
             this.label4.Name = "label4";
@@ -208,6 +212,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(58, 193);
             this.label3.Name = "label3";
@@ -218,6 +223,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(54, 119);
             this.label1.Name = "label1";
@@ -228,6 +234,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(54, 52);
             this.label2.Name = "label2";
@@ -270,6 +277,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1400, 688);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
--- signup.cs
+++ signup.cs
@@ -67,7 +67,7 @@ namespace trial_transportation
 
 
 
-            StreamReader s1 = new StreamReader(@"D:\1-2\SWE4202\project\Travel-Management-system\user.txt");
+            StreamReader s1 = new StreamReader(@"C:\Users\User\OneDrive\Desktop\New folder (5)\Travel-Management-system\user.txt");
             string line = "";
             while ((line = s1.ReadLine()) != null)
             {
@@ -136,7 +136,7 @@ namespace trial_transportation
                     
                    
 
-                    string path = @"D:\1-2\SWE4202\project\Travel-Management-system\user.txt";
+                    string path = @"C:\Users\User\OneDrive\Desktop\New folder (5)\Travel-Management-system\user.txt";
                     bool use = true;
 
 
